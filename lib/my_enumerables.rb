e2ebaba2024509d6d608e01d60b7e9da37d0e7cb
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-23 19:32:54 +0100

    feat: Complete my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Custom Enumerable Project
 module Enumerable
   # Your code goes here
 end
@@ -7,5 +10,29 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  # Iterate over the elements of `self`
+  def my_each
+    if block_given?
+      size.times do |idx|
+        yield self[idx]
+      end
+    end
+    self
+  end
 end
+
+# Test array
+arr = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+arr2 = %w[apple banana cherry]
+# arr.each { |elem| p elem }
+# arr.my_each { |elem| p elem }
+# result = arr2.my_each { |fruit| puts "I like #{fruit}." }
+# puts result.inspect
+
+# result = 3.times.with_index { |i| puts i }
+# puts result.class
+# puts result.inspect
+
+# original = [1, 2, 3].each { |num| puts num }
+# puts original.class
+# puts original.inspect
